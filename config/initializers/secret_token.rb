d5e955b047bb6bf4a57a19571ea11e8b2602dbfb
Bryana Knight <brknig11@gmail.com>
2013-11-25 09:57:17 -0700

    Generate initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ContactManager::Application.config.secret_key_base = 'd234812c08b0261d84139c865c5f32c62649bb351132ee10306bd0f67e2b24e6cff7c1fd68183d978141cd1961fd3af2079fb05dd80b6175515c3698784f8e7f'
